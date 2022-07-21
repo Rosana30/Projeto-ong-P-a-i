40a0d82bf1c7eba1ed43fff5ee2e2e235221fbd4
Rosana30 <rosanasouza2@hotmail.com>
2022-07-21 20:14:22 -0300

    criacao de update delete

--- src/controllers/gatoControlles.js
+++ src/controllers/gatoControlles.js
@@ -71,7 +71,90 @@ const getFindGatos = async (req, res) => {
   }
 };
 
+
+
+// Update gato  by the id in the request
+const updateGato = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update!"
+    });
+  }
+
+  const id = req.params.id;
+
+  gatoModel.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Gato with id=${id}. !`
+        });
+      } else res.send({ message: "Gato was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Gato with id=" + id
+      });
+    });
+};
+
+// Delete Gato with the specified id in the request
+const deleteGato = (req, res) => {
+  const id = req.params.id;
+
+  gatoModel.findByIdAndRemove(id)
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Gato with id=${id}. Maybe Gato was not found!`
+        });
+      } else {
+        res.send({
+          message: "Gato was deleted successfully!"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "nao foi possivel excluir o gato id=" + id
+      });
+    });
+};
+
+// Delete todos os gatos from the database.
+const deleteAll = (req, res) => {
+  Gato.deleteMany({})
+    .then(data => {
+      res.send({
+        message: `${data.deletedCount} Gatos were deleted successfully!`
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all Cultivas."
+      });
+    });
+};
+
+// Find all published Gatos
+// exports.findAllPublished = (req, res) => {
+//   Gato.find({ published: true })
+//     .then(data => {
+//       res.send(data);
+//     })
+//     .catch(err => {
+//       res.status(500).send({
+//         message:
+//           err.message || "Some error occurred while retrieving Gatos."
+//       });
+//     });
+// };
+
 module.exports = {
   createGato,
   getFindGatos,
+  updateGato,
+  deleteGato,
+  deleteAll
 };
--- src/routes/gatosRoutes.js
+++ src/routes/gatosRoutes.js
@@ -4,6 +4,10 @@ const rota = app.Router()
 
 rota.post("/create", controllers.createGato)
 
+rota.put("/update/:id", controllers.updateGato)
+
+rota.delete("/excluir/:id",controllers.deleteGato)
+
 
 rota.get('/gato', controllers.getFindGatos)
 

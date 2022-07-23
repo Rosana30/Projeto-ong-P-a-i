2b06d482893da9169e9e523caa7cdeafbb6bb730
Rosana30 <rosanasouza2@hotmail.com>
2022-07-23 13:58:08 -0300

    Procfile

--- src/models/gatoSchema.js
+++ src/models/gatoSchema.js
@@ -1,15 +1,15 @@
-const mongoose =  require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-let GatoSchema = new Schema ({
-    _id: {
-        type: mongoose.Types.ObjectId,
-        default: mongoose.Types.ObjectId
-    },  
-    nome: { type: String, required: true },
-    sexo: { type: String, required: true },
-    idade:{ type: String, required: true},
-    local:{ type: String, required: true },
 
-    
- })
- module.exports = mongoose.model("gato", GatoSchema)
+let GatoSchema = new Schema({
+  _id: {
+    type: mongoose.Types.ObjectId,
+    default: mongoose.Types.ObjectId,
+  },
+  nome: { type: String, required: true },
+  sexo: { type: String, required: true },
+  idade: { type: String, required: true },
+  local: { type: String, required: true },
+});
+
+module.exports = mongoose.model("gato", GatoSchema);
